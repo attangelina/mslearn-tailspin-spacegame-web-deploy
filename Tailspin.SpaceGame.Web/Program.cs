8545da269704fa13c5a1f192bef061ae5680a46a
Angelina Gerbas <ag150q@att.com>
2021-03-29 13:16:39 -0500

    Added a few log statements

--- Tailspin.SpaceGame.Web/Program.cs
+++ Tailspin.SpaceGame.Web/Program.cs
@@ -1,14 +1,24 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace TailSpin.SpaceGame.Web
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            var logger = host.Services.GetRequiredService<ILogger<Program>>();
+            logger.LogInformation("Angelina's log - info");
+            logger.LogWarning("Angelina's log - warning");
+            logger.LogError("Angelina's log - error");
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
